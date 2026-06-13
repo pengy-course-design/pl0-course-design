8e205c9a44f3713459b6ecd6e8c026774255605d
hanlian <pengy9883@gmail.com>
2026-06-13 19:54:46 +0800

    feat(web): add Spring Boot entry and home page

---
+++ src/main/java/com/pengy/pl0/Pl0CourseDesignApplication.java
@@ -0,0 +1,12 @@
+package com.pengy.pl0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pl0CourseDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pl0CourseDesignApplication.class, args);
+    }
+}
